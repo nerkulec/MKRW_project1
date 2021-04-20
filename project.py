27143da3d1c55fb1a0c568bf0f474fb15ef127de
nerkulec <nerkulec@github.com>
2021-04-20 19:50:02 +0200

    sgd

--- project.py
+++ project.py
@@ -3,6 +3,7 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.decomposition import NMF
 import numpy as np
 import pandas as pd
+from tqdm import tqdm, trange
 
 # %%
 import argparse
@@ -58,10 +59,22 @@ def RMSE(prediction, truth):
 
 
 # %%
-def nmf(matrix, r, max_iter=1000):
+def nmf(Z: np.ndarray, r: int = 7, max_iter: int = 1000) -> np.ndarray:
+    """
+    Function performs low rank approximation of utility matrix Z via Non-negative Matrix Factorization.
+
+    :param Z: Utility matrix Z (users x movies)
+    :type Z: numpy.ndarray
+    :param r: Number of components in NMF (ranks)
+    :type r: int
+    :param max_iter: Maximum number of NMF algorithm iterations
+    :type max_iter: int
+    :return: Rank r matrix - an approximation of Z.
+    :rtype: numpy.ndarray
+    """
     model = NMF(n_components=r, init='random',
                 random_state=0, max_iter=max_iter)
-    W = model.fit_transform(matrix)
+    W = model.fit_transform(Z)
     H = model.components_
     Z_approximated = np.dot(W, H)
     return Z_approximated
@@ -118,48 +131,127 @@ def svd_2(Z: np.ndarray, r: int, Z_test: np.ndarray, n_iter: int = 100, update: 
     return Zr
 
 
-def fill_zeros(matrix):
-    return np.nan_to_num(matrix, nan=0.0)
+def sgd(Z: np.ndarray, r: int = 7, max_iter: int = 200, alpha: float = 0.0003, lambd: float = 0.01) -> np.ndarray:
+    """
+    Function performs low rank approximation of utility matrix Z via SGD.
+
+    :param Z: Utility matrix Z (users x movies) (may have missing values)
+    :type Z: numpy.ndarray
+    :param r: Number of components in SGD (ranks)
+    :type r: int
+    :param max_iter: Maximum number of SGD algorithm iterations
+    :type max_iter: int
+    :return: Rank r matrix - an approximation of Z.
+    :rtype: numpy.ndarray
+    """
+    n, d = Z.shape
+    W = np.random.normal(size=(n, r))
+    H = np.random.normal(size=(r, d))
+    
+    not_nans = np.argwhere(~np.isnan(Z))
+    
+    def loss(W, H, truth):
+        s = 0
+        for (user_id, movie_id) in not_nans:
+            w_i = W[user_id,:]
+            h_j = H[:,movie_id]
+            s += (w_i.T@h_j-truth[user_id, movie_id])**2 + lambd*((w_i**2).sum()+(h_j**2).sum())
+        return s
+
+    def grad(W, H, truth):
+        DW = np.zeros(shape=(n, r))
+        DH = np.zeros(shape=(r, d))
+        for (user_id, movie_id) in not_nans:
+            w_i = W[user_id,:]
+            h_j = H[:,movie_id]
+            t_i_j = truth[user_id, movie_id]
+            DW[user_id, :] += 2*(w_i.T@h_j-t_i_j)*h_j + lambd*2*w_i
+            DH[:, movie_id] += 2*(h_j.T@w_i-t_i_j)*w_i + lambd*2*h_j
+        return DW, DH
+        
+    for i in trange(max_iter):
+        DW, DH = grad(W, H, Z)
+        W = W - alpha*DW
+        H = H - alpha*DH
+    
+    Z_approximated = np.dot(W, H)
+    return Z_approximated
+  
+
+def fill_zeros(Z):
+    """
+    Fills missing values in Z with zeros
+
+    :param Z: Utility matrix Z (users x movies)
+    :type Z: numpy.ndarray
+    :return: matrix Z with NaNs replaced with zeros
+    :rtype: numpy.ndarray
+    """
+    return np.nan_to_num(Z, nan=0.0)
 
 
-def fill_mean_global(matrix):
-    m = np.nanmean(matrix)
-    return np.nan_to_num(matrix, nan=m)
+def fill_mean_global(Z):
+    """
+    Fills missing values in Z with global mean
+
+    :param Z: Utility matrix Z (users x movies)
+    :type Z: numpy.ndarray
+    :return: matrix Z with NaNs replaced with global mean
+    :rtype: numpy.ndarray
+    """
+    m = np.nanmean(Z)
+    return np.nan_to_num(Z, nan=m)
 
 
-def fill_mean_movies(matrix):
-    col_mean = np.nanmean(matrix, axis=0)
+def fill_mean_movies(Z):
+    """
+    Fills missing values in Z with means per movie
+
+    :param Z: Utility matrix Z (users x movies)
+    :type Z: numpy.ndarray
+    :return: matrix Z with NaNs replaced with means per movie
+    :rtype: numpy.ndarray
+    """
+    col_mean = np.nanmean(Z, axis=0)
     col_mean = np.nan_to_num(col_mean, nan=0.0)
-    inds = np.where(np.isnan(matrix))
-    matrix_copy = matrix.copy()
-    matrix_copy[inds] = np.take(col_mean, inds[1])
-    return matrix_copy
+    inds = np.where(np.isnan(Z))
+    Z_copy = Z.copy()
+    Z_copy[inds] = np.take(col_mean, inds[1])
+    return Z_copy
 
 
-def fill_mean_users(matrix):
-    row_mean = np.nanmean(matrix, axis=1)
+def fill_mean_users(Z):
+    """
+    Fills missing values in Z with means per user
+
+    :param Z: Utility matrix Z (users x movies)
+    :type Z: numpy.ndarray
+    :return: matrix Z with NaNs replaced with means per user
+    :rtype: numpy.ndarray
+    """
+    row_mean = np.nanmean(Z, axis=1)
     row_mean = np.nan_to_num(row_mean, nan=0.0)
-    inds = np.where(np.isnan(matrix))
-    matrix_copy = matrix.copy()
-    matrix_copy[inds] = np.take(row_mean, inds[0])
-    return matrix_copy
+    inds = np.where(np.isnan(Z))
+    Z_copy = Z.copy()
+    Z_copy[inds] = np.take(row_mean, inds[0])
+    return Z_copy
 
 
 # %%
 # Experiment 1
-filled_train_ratings = fill_mean_users(train_ratings)
+filled_ratings = fill_mean_users(train_ratings)
 
 if args.alg == 'NMF':
     approximation = nmf(filled_ratings, 6, max_iter=1000)
 elif args.alg == 'SVD1':
     approximation = svd_1(filled_ratings, 6)
 elif args.alg == 'SVD2':
-    approximation = svd_2(Z=train_ratings, r=7,
+    approximation = svd_2(Z=filled_ratings, r=7,
                           Z_test=test_ratings, n_iter=100, update=10)
 elif args.alg == 'SGD':
-    pass
+    approximation = sgd(train_ratings)
 
 rmse_test = RMSE(approximation, test_ratings)
 
 with open(args.result, 'a') as f:
-    f.write(str(rmse_test))
+    f.write(f'{args.alg}: {rmse_test:.2f}\n')
---
+++ run_SVD2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 project.py --train train_ratings.csv --test test_ratings.csv --alg SVD2 --result res.txt
